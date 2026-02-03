f0efc14b1a29185ba292de589504b9a7eb99a56a
Umar Bisharat <muhammadumarjatt303@gmail.com>
2026-02-03 13:20:32 +0500

    Create volume.c

---
+++ 5.Week 4 - Memory/volume.c
@@ -0,0 +1,56 @@
+// Modifies the volume of an audio file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "rb");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // Read samples from input file and write updated samples to output file
+    // Each sample is a 16-bit signed integer
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
+    {
+        // Scale the sample by the factor
+        buffer = (int16_t)(buffer * factor);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+
+    return 0;
+}
